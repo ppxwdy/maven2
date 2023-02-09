e2ca7b73d121aca4c301dcae50a99c045c6aa71a
KyrieW <dxdbwym@163.com>
2023-02-09 18:44:58 +0800

    modify the README.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # maven2
 create the project
 
+ssss
 2023.2.9
--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class hellow {
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
 }
